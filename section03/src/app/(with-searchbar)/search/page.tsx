cdaa9fe28a693812350f12aaee572cdd0b0645b1
rondido <rppr001@naver.com>
2025-02-28 17:31:37 +0900

    layout 설정 및 네비게이션 관련 강의

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,3 +1,6 @@
+import ClientComponent from "@/components/client-component";
+import { Searchbar } from "@/components/searchbar";
+
 //서버 컴포넌트이기 때문에 async가 사용 가능
 const Page = async ({
   searchParams,
@@ -5,7 +8,14 @@ const Page = async ({
   searchParams: Promise<{ q: string }>;
 }) => {
   const { q } = await searchParams;
-  return <div>설치 페이지 {q}</div>;
+  return (
+    <div>
+      설치 페이지 {q}
+      {/* <ClientComponent> */}
+      <Searchbar />
+      {/* </ClientComponent> */}
+    </div>
+  );
 };
 
 export default Page;
