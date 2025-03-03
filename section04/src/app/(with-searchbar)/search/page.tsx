4ea8cd4423e3a8c6016d51b9d2ba1f2b3e2d600a
rondido <rppr001@naver.com>
2025-03-04 00:34:28 +0900

    클라이언트 캐시 강의

--- section04/src/app/(with-searchbar)/search/page.tsx
+++ section04/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,15 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 
+// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
+// 1. auto : 기본값, 아무것도 강제하지 않음 (생략 가능)
+// 2. force-dynamic : 페이지를 강제로 Dynamic 페이지로 설정
+// 3. force-static: 페이지를 강제로 static 페이지로 설정
+// 4. error : 페이지를 강제로 static 페이지로 설정(설정하면 안되는 이유 -> 빌드 오류)
+//동적인 페이지의 경우 static으로 사용할 경우 정상적으로 작동하지 않는다.
+//export const dynamic = "";
+//권장하지 않는다.
+
 export default async function Page({
   searchParams,
 }: {
